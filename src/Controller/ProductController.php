e4c9a76da2e06610f341bb0790ac5a2c4df3a58d
Anthony <acs.agl46@gmail.com>
2021-03-16 18:44:36 +0100

    Finishes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Product;
 use OpenApi\Annotations as OA;
-use Nelmio\ApiDocBundle\Annotation\Model;
 use JMS\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Contracts\Cache\ItemInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations\View;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class ProductController extends AbstractFOSRestController
@@ -40,7 +45,7 @@ class ProductController extends AbstractFOSRestController
      * 
      * @OA\Response(
      *     response=200,
-     *     description="Retourne les informations sur un produit. Prix en euros (€)",
+     *     description="Return all informations of a product. Price in euros (€)",
      *     @OA\JsonContent(
      *        type="array",
      *        @OA\Items(ref=@Model(type=Product::class, groups={"detail"}))
@@ -50,11 +55,11 @@ class ProductController extends AbstractFOSRestController
      * @OA\Parameter(
      *     name="id",
      *     in="path",
-     *     description="Id d'un produit.",
+     *     description="Product ID.",
      *     @OA\Schema(type="int")
      * )
      * 
-     * @OA\Tag(name="Produits")
+     * @OA\Tag(name="Products")
      * 
      * @param Product $product
      * @return Product $product
@@ -66,12 +71,12 @@ class ProductController extends AbstractFOSRestController
 
 
     /**
-     * @Rest\Get("/api/products")
+     * @Rest\Get("/api/user/{id}/products")
      * @View()
      * 
      * @OA\Response(
      *     response=200,
-     *     description="Retourne la liste de vos produits. Prix en euros (€)",
+     *     description="Return your product list. Price in euros (€)",
      *     @OA\JsonContent(
      *        type="array",
      *        @OA\Items(ref=@Model(type=Product::class, groups={"list"}))
@@ -79,36 +84,56 @@ class ProductController extends AbstractFOSRestController
      * )
      * 
      * @OA\Parameter(
-     *     name="page",
-     *     in="query",
-     *     description="Numéro de la page (10 produits par page)",
+     *     name="id",
+     *     in="path",
+     *     description="Your client ID",
      *     @OA\Schema(type="int")
      * )
      * 
-     * @OA\Tag(name="Produits")
+     * @OA\Parameter(
+     *     name="page",
+     *     in="query",
+     *     description="Number of page (10 products per page)",
+     *     @OA\Schema(type="int")
+     * )
+     * 
+     * @OA\Tag(name="Products")
      * 
      * @param Request $request
      * @param PaginatorInterface $paginator
      * @return Object
      */
-    public function getProductsList(Request $request, PaginatorInterface $paginator)
+    public function getProductsList(User $user, Request $request, PaginatorInterface $paginator)
     {
+        if ($user != $this->getUser()) {
+            throw new Exception('Acces denied');
+        }
+
         $nbPage = intval($request->get('page'));
         
-        if($nbPage == null) {
+        if ($nbPage == null) {
             $nbPage = 1;
         }
 
-        $allDatas = $this->entityManager->getRepository(Product::class)->getUserProducts($this->getUser());
-        $allDatas = $this->serialize->serialize($allDatas, 'json', SerializationContext::create()->setGroups(array('list')));
-        $allDatas = $this->serialize->deserialize($allDatas, 'array', 'json');
+        $cacheName = 'all_products_list_'.$user->getId().'_'.$nbPage;
+        $cache = new FilesystemAdapter();
 
-        $data = $paginator->paginate(
-            $allDatas,
-            $nbPage,
-            10
-        );
+        $values = $cache->get($cacheName, function (ItemInterface $item) use ($paginator, $nbPage, $user) {
+            $item->expiresAfter(3600);
+        
+            $allDatas = $this->entityManager->getRepository(Product::class)->getUserProducts($this->getUser());
+            $allDatas = $this->serialize->serialize($allDatas, 'json', SerializationContext::create()->setGroups(array('list')));
+            $allDatas = $this->serialize->deserialize($allDatas, 'array', 'json');
 
-        return $data;
+            $data = $paginator->paginate(
+                $allDatas,
+                $nbPage,
+                10
+            );
+        
+            return $data;
+        });
+
+        return $values;
     }
 }
